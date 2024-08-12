63cc56ccb10e157f7468f6bcd071fce76536144c
juliachaush <juliachaush@gmail.com>
2024-08-12 09:53:01 +0300

    add path

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    API_PATH: process.env.API_PATH,
-  },
+  //   env: {
+  //     API_PATH: process.env.API_PATH,
+  //   },
 };
 
 export default nextConfig;
--- src/lib/products.js
+++ src/lib/products.js
@@ -1,8 +1,9 @@
-const apiUrl = process.env.API_PATH;
+// const apiUrl = process.env.API_PATH;
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3001";
 
 async function fetchProducts() {
   try {
-    const response = await fetch(`${apiUrl}/products`, {
+    const response = await fetch(`${baseUrl}/api/products`, {
       mode: "no-cors",
     });
 
