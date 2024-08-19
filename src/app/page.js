27e2817c53267862576fdac790530010aa66702a
juliachaush <juliachaush@gmail.com>
2024-08-19 20:50:34 +0300

    add main photo

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,27 @@
 import MainHeader from "../components/MainHeader";
+import mainPhoto from "../assets/main.jpg";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-      <MainHeader
-        path={{ products: "/products", contacts: "/contacts" }}
-        linkName={{ products: "Catalog", contacts: "Contacts" }}
-        cart={{ path: "/cart", name: "Cart" }}
-      />
+      <div className="fixed top-0 left-0 w-full  z-10">
+        <MainHeader
+          path={{ products: "/products", contacts: "/contacts" }}
+          linkName={{ products: "Catalog", contacts: "Contacts" }}
+          cart={{ path: "/cart", name: "Cart" }}
+        />
+      </div>
+      <div className="relative w-screen h-screen">
+        <Image
+          src={mainPhoto}
+          alt="Photo wit candle"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="mt-50"
+        />
+      </div>
     </main>
   );
 }
--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,13 +1,15 @@
+"use client";
+
 import Link from "next/link";
 
 function MainHeader({ path, linkName, cart }) {
   return (
     <>
-      <div className=" flex flex-row bg-gray-950 text-white w-full  justify-center  ">
+      <div className=" flex flex-row bg-gray-950 text-white w-full  justify-center text-sm  ">
         <p> Free shipping from 50$</p>
       </div>
-      <div className="flex flex-row bg-gray-10 text-black mb-2 items-center spa justify-between border-b border-gray-950 tracking-widest ">
-        <div className="flex flex-row list-none w-2 ">
+      <div className="flex flex-row bg-white text-black mb-2 items-center spa justify-between border-b border-gray-950 tracking-widest ">
+        <div className="flex flex-row list-none w-2  ">
           {Object.keys(path).map((key) => (
             <li key={key} className="p-4">
               <Link href={path[key]}>{linkName[key]}</Link>
