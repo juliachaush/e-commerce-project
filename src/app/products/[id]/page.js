50ae10b38dd7305f53fe9e2324ffb18846f4ee99
juliachaush <juliachaush@gmail.com>
2024-08-31 13:21:55 +0300

    add meta data on product page

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -4,6 +4,117 @@ import { Footer } from "@/src/components/Footer";
 import { ProductCardPage } from "@/src/components/ProductCardPage";
 import Wrapper from "@/src/components/Wrapper";
 
+export async function generateMetadata({ params }) {
+  const { id } = params;
+  const product = await fetchProductById(id);
+
+  const breadcrumbSchema = {
+    "@context": "https://schema.org",
+    "@type": "BreadcrumbList",
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        position: 1,
+        name: "Home",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      },
+      {
+        "@type": "ListItem",
+        position: 2,
+        name: "Products",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}/products`,
+      },
+      {
+        "@type": "ListItem",
+        position: 3,
+        name: "Product Details",
+        item: `${process.env.NEXT_PUBLIC_BASE_URL}/products/${id}`,
+      },
+    ],
+  };
+
+  if (!product) {
+    return {
+      title: "Product Not Found",
+      description: "The requested product could not be found.",
+      openGraph: {
+        title: "Product Not Found",
+        description: "The requested product could not be found.",
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/products/not-found`,
+        type: "website",
+      },
+      twitter: {
+        card: "summary_large_image",
+        title: "Product Not Found",
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/products/not-found`,
+        description: "The requested product could not be found.",
+      },
+      alternates: {
+        canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/products/not-found`,
+      },
+      structuredData: {
+        "@context": "https://schema.org",
+        "@type": "Product",
+        name: "Product Not Found",
+        image: "summary_large_image",
+        description: "The requested product could not be found.",
+        offers: {
+          "@type": "Offer",
+          priceCurrency: "USD",
+          price: "product price not be found.",
+          availability: "https://schema.org/InStock",
+        },
+      },
+      breadcrumb: breadcrumbSchema,
+    };
+  }
+
+  return {
+    title: `${product[0].product_title} - Product Details`,
+    description: `Find out more about ${product[0].product_title}. Available for $${product[0].product_price}.`,
+    openGraph: {
+      type: "website",
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/products/${id}`,
+      title: `${product[0].product_title} - Product Details`,
+      description: `Find out more about ${product[0].product_title}. Available for $${product[0].product_price}.`,
+      site_name: "Base Code",
+      locale: "en_US",
+      images: [
+        {
+          url: product[0].image_url,
+          width: 800,
+          height: 600,
+          alt: product[0].product_title,
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      title: `${product[0].product_title} - Product Details`,
+      description: `Find out more about ${product[0].product_title}. Available for $${product[0].product_price}.`,
+      images: [product[0].image_url],
+    },
+    alternates: {
+      canonical: `${process.env.NEXT_PUBLIC_BASE_URL}/products/${id}`,
+    },
+    structuredData: {
+      "@context": "https://schema.org",
+      "@type": "Product",
+      name: product[0].product_title,
+      image: product[0].image_url,
+      description: `Find out more about ${product[0].product_title}. Available for $${product[0].product_price}.`,
+      offers: {
+        "@type": "Offer",
+        priceCurrency: "USD",
+        price: product[0].product_price,
+        availability: "https://schema.org/InStock",
+      },
+    },
+    breadcrumb: breadcrumbSchema,
+  };
+}
+
 export default async function ProductPage({ params, onClick }) {
   const { id } = params;
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -60,28 +60,6 @@ const Input = () => {
         )}
       </div>
     </form>
-
-    // <form onSubmit={handleSubmit}>
-    //   <div className=" relative grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1  lg:grid-rows-1 md:grid-rows-1 sm:grid-rows-2 lg:gap-0 md:gap-0 sm:gap-2   ">
-    //     <input
-    //       id="email"
-    //       name="email"
-    //       type="email"
-    //       placeholder="email"
-    //       className=" border border-gray-950 text-gray-950 bg-gray-100 p-2 pl-4 min-w-[160px] placeholder-gray-950 focus:outline-none focus:border-gray-500 focus:ring-0 placeholder-hidden"
-    //       value={email}
-    //       onChange={(e) => setEmail(e.target.value)}
-    //     />
-    //     <button type="submit" className="border border-gray-950 text-gray-950 ">
-    //       {BUTTON_NAMES.subscribeButton}
-    //     </button>
-    //     {message && (
-    //       <div className="  absolute mt-10 text-red-600 text-sm  ">
-    //         <p>{message}</p>
-    //       </div>
-    //     )}
-    //   </div>
-    // </form>
   );
 };
 
